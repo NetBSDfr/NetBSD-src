44c287de0f541e748a448ac1d1387d11bfcf8f58
Emile 'iMil' Heitor <imil@NetBSD.org>
2024-01-08 08:57:01 +0100

    fix: sync with trunk

--- sys/dev/pci/virtio.c
+++ sys/dev/pci/virtio.c
@@ -74,8 +74,7 @@ static void	virtio_reset_vq(struct virtio_softc *,
 void
 virtio_set_status(struct virtio_softc *sc, int status)
 {
-	if (sc->sc_ops->set_status)
-		sc->sc_ops->set_status(sc, status);
+	sc->sc_ops->set_status(sc, status);
 }
 
 /*
@@ -817,7 +816,6 @@ virtio_alloc_vq(struct virtio_softc *sc, struct virtqueue *vq,
 	/* alloc and map the memory */
 	r = bus_dmamem_alloc(sc->sc_dmat, allocsize, VIRTIO_PAGE_SIZE, 0,
 	    &vq->vq_segs[0], 1, &rsegs, BUS_DMA_WAITOK);
-
 	if (r != 0) {
 		aprint_error_dev(sc->sc_dev,
 		    "virtqueue %d for %s allocation failed, "
@@ -893,6 +891,7 @@ virtio_alloc_vq(struct virtio_softc *sc, struct virtqueue *vq,
 		    "descriptors\n", size_indirect, maxnsegs * vq_num);
 
 	return 0;
+
 err:
 	sc->sc_ops->setup_queue(sc, vq->vq_index, 0);
 	if (vq->vq_dmamap)
