6a3fad42bd0add5b9d7a30a7a423638b9c26f0a4
Emile 'iMil' Heitor <imil@NetBSD.org>
2024-01-19 11:39:07 +0100

    feat: multiline support

--- tsenable.py
+++ tsenable.py
@@ -1,15 +1,21 @@
 import re
 import sys
 
-keywords = ["defined", "sizeof", "return", "while", "calc_cache_size", "fork1",
-        "panic", "ctob"]
+keywords = ["defined", "sizeof", "return", "while", "panic"]
+multiline = [ "calc_cache_size", "fork1", "ctob"]
 
 with open(sys.argv[1], 'r') as file:
    lines = file.readlines()
 
 in_section = False
+exit_func = None
 for line in lines:
-    if 'TSENTER()' in line:
+    if exit_func:
+        print(line.rstrip())
+        print(exit_func)
+        exit_func = None
+        continue
+    elif 'TSENTER()' in line:
         in_section = True
         print(line.rstrip())
     elif 'TSEXIT()' in line:
@@ -25,7 +31,11 @@ for line in lines:
             s, f = m[0]
             print(f'{s}TSENTER2("{f}");')
             print(line.rstrip())
-            print(f'{s}TSEXIT2("{f}");\n')
+            exit_func = f'{s}TSEXIT2("{f}");\n'
+            if any(l in line for l in multiline):
+                continue;
+            print(exit_func)
+            exit_func = None
         else:
             print(line.rstrip())
     else:
