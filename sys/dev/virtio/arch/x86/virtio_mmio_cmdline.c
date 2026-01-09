738961b50c77b02afa71bb46f82ef02cf934d4ee
Emile 'iMil' Heitor <imil@NetBSD.org>
2026-01-09 07:28:16 +0100

    rework the command line processing

--- sys/dev/virtio/arch/x86/virtio_mmio_cmdline.c
+++ sys/dev/virtio/arch/x86/virtio_mmio_cmdline.c
@@ -79,6 +79,13 @@ struct virtio_mmio_cmdline_softc {
 	struct mmio_args		margs;
 };
 
+static struct {
+	char cmdline_copy[LINE_MAX];
+	char *current_pos;
+	bool initialized;
+} cmdline_state = { "", NULL, false };
+
+static void	cmdline_init(void);
 static int	virtio_mmio_cmdline_match(device_t, cfdata_t, void *);
 static void	virtio_mmio_cmdline_attach(device_t, device_t, void *);
 static int	virtio_mmio_cmdline_do_attach(device_t,
@@ -94,34 +101,30 @@ CFATTACH_DECL3_NEW(mmio_cmdline,
     virtio_mmio_cmdline_detach, NULL,
     virtio_mmio_cmdline_rescan, NULL, 0);
 
-static char *parg = NULL;
 
 static void
-copy_cmdline(bool empty)
+cmdline_init(void)
 {
-	static char cmdline_copy[LINE_MAX] = "";
-
-	if (!empty) {
-		cmdline_copy[0] = 0;
+	if (cmdline_state.initialized)
 		return;
-	}
-	if (!strlen(cmdline_copy)) {
-		strlcpy(cmdline_copy, xen_start_info.cmd_line, sizeof(cmdline_copy));
-		parg = strstr(cmdline_copy, VMMIOSTR);
-	}
+
+	strlcpy(cmdline_state.cmdline_copy, xen_start_info.cmd_line,
+		sizeof(cmdline_state.cmdline_copy));
+	cmdline_state.current_pos = strstr(cmdline_state.cmdline_copy, VMMIOSTR);
+	cmdline_state.initialized = true;
 }
 
 static int
 virtio_mmio_cmdline_match(device_t parent, cfdata_t match, void *aux)
 {
-	static bool start_match = true;
+	static bool first_match_call = true;
 
-	/* When matching, dont refill cmdline_copy when exhausted. */
-	if (start_match) {
-		copy_cmdline(true);
-		start_match = false;
+	if (first_match_call) {
+		first_match_call = cmdline_state.initialized = false;
+		cmdline_init();
 	}
-	if (parg == NULL || strstr(parg, VMMIOSTR) == NULL)
+
+	if (cmdline_state.current_pos == NULL)
 		return 0;
 
 	return 1;
@@ -214,41 +217,36 @@ mmio_args_parse(struct mmio_args *margs)
 	int keylen = strlen(VMMIOSTR);
 	char *next;
 
-	/* first pass, or emptied parg from last pass */
-	if (parg == NULL)
-		copy_cmdline(true);
+	cmdline_init();
 
 	/* no args were found */
-	if (parg == NULL) {
+	if (cmdline_state.current_pos == NULL) {
 		return MMIO_NO_ARG;
 	}
 
 	/* useless, no parameters */
-	if (strlen(parg) <= keylen) {
-		parg = NULL;
+	if (strlen(cmdline_state.current_pos) <= keylen) {
+		cmdline_state.current_pos = NULL;
 		return MMIO_NO_ARG;
 	}
 
-	parg += keylen;
+	cmdline_state.current_pos += keylen;
 
-	next = parg;
+	next = cmdline_state.current_pos;
 	while (*next && *next != ' ') /* find end of argument */
 		next++;
 	if (*next) { /* space */
 		*next++ = '\0'; /* end the argument string */
 		next = strstr(next,VMMIOSTR);
 	}
-	parsearg(margs, parg);
+	parsearg(margs, cmdline_state.current_pos);
 
-	if (next != NULL)
-		parg = next;
-
-	if (!*parg) {
-		parg = NULL;
-		/* Empty cmdline for next pass (match) */
-		copy_cmdline(false);
+	if (next == NULL || !*next) {
+		cmdline_state.current_pos = NULL;
 		return MMIO_LAST_ARG;
 	}
+
+	cmdline_state.current_pos = next;
 
 	return MMIO_NEXT_ARG;
 }
