84c5a0c056d29255765e094dc01df9dfd0c9ab4f
Emile 'iMil' Heitor <imil@NetBSD.org>
2026-01-16 10:54:51 +0100

    identify smol custom kernel

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -228,7 +228,7 @@ cwd=$(dirname "$0")
 copyright="$(cat "${cwd}/copyright")"
 osrelcmd=${cwd}/osrelease.sh
 
-ost="NetBSD"
+ost="smolBSD"
 osr=$(sh $osrelcmd)
 
 fullversion="${ost} ${osr} (${id})${reproversion}\n"
