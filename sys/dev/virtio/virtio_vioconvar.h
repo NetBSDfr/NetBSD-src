59991624d5d9ac95c555b895b5fc6a51fc9a1d91
Emile 'iMil' Heitor <imil@NetBSD.org>
2026-01-21 07:56:12 +0100

    viocon(4) early console support

---
+++ sys/dev/virtio/virtio_vioconvar.h
@@ -0,0 +1,9 @@
+
+#ifndef _VIRTIO_VIOCONVAR_H_
+#define _VIRTIO_VIOCONVAR_H_
+
+#include <sys/bus.h>
+
+int viocon_earlyinit(bus_space_tag_t, bus_space_handle_t);
+
+#endif /* _VIRTIO_VIOCONVAR_H_ */
